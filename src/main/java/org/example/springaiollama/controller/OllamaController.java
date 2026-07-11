23616c77acbd624e84c531ef4255be336cf30f1e
Naresh-Celestial <naresh.sharma@celestialsys.com>
2026-07-11 18:49:56 +0530

    First Commit

---
+++ src/main/java/org/example/springaiollama/controller/OllamaController.java
@@ -0,0 +1,23 @@
+package org.example.springaiollama.controller;
+
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.DefaultChatClientBuilder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class OllamaController {
+  
+  ChatClient chatClient;
+
+  public OllamaController(ChatClient.Builder chatClientBuilder) {
+    this.chatClient = chatClientBuilder.build();
+  }
+
+  @GetMapping("/ask/ollama/{userText}")
+  public String askOllama(@PathVariable String userText){
+    
+    return chatClient.prompt(userText).call().content();
+  }
+}
